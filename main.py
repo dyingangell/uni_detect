9f688fe71df30fb890f4c3cfaf0b1486f96e5b82
luc1k1 <rasul27092007@gmail.com>
2026-03-20 16:55:47 +0400

    a

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ SIZE = int(200 * 640 * 640 * 3)
 def start_master():
     try:
         shm = shared_memory.SharedMemory(name=SHM_NAME, create=True, size=SIZE)
-        print(f"✅ Память '{SHM_NAME}' создана и УДЕРЖИВАЕТСЯ.")
-        print("НЕ ЗАКРЫВАЙТЕ это окно, пока работают воркеры!")
+        print(f"✅ Shared Memory '{SHM_NAME}' created and held.")
+        print("Don't close this terminal window until workers are done!")
 
-        # Бесконечный цикл, чтобы процесс не умер
+        # Infinite loop to keep the process alive
         while True:
             time.sleep(10)
 
     except FileExistsError:
-        print("Память уже создана кем-то другим.")
+        print("Shared Memory already exists.")
     except KeyboardInterrupt:
-        print("\nУдаление памяти и выход...")
+        print("\nDeleting Shared Memory and exiting...")
         shm.close()
         shm.unlink()
 
